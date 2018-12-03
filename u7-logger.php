468b592f16350575fad6758517baa28c57180d0e
Anatoly Yumashev <a.yumashev@gmail.com>
2018-12-03 12:48:39 +0300

    Update u7-logger.php

--- u7-logger.php
+++ u7-logger.php
@@ -6,51 +6,77 @@ Author: WPCraft
 Author URI: https://wpcraft.ru/
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
-Version: 1.3
+Version: 1.4
 */
-register_uninstall_hook( __FILE__, 'logger_u7_uninstall' );
-function logger_u7_uninstall() {
-	delete_option( 'logger_u7' );
-}
 
-class Logger_U7 {
-	public $url;
-	
+namespace uptimizt;
+
+/**
+ * Logger data for debug
+ */
+class Logger {
+
+  /**
+   * Save url
+   */
+	public static $url;
+
 	/**
-	 * Logger_U7 constructor.
+	 * The Init
 	 */
-	public function __construct() {
-		$this->url = $_SERVER['REQUEST_URI'];
+	public static function init() {
+		self::$url = $_SERVER['REQUEST_URI'];
 		add_action( 'admin_menu', function () {
-			add_management_page( $page_title = 'Logger', $menu_title = 'Logger', $capability = 'manage_options', $menu_slug = 'logger_u7', $func = array(
-				$this,
-				'display_page',
-			) );
-		} );
-		add_filter( "plugin_action_links_" . plugin_basename( __FILE__ ), array( $this, 'add_settings_link' ) );
-		add_action( 'tool_btns', array( $this, 'manual_clear' ), 15 );
-		add_action( 'tool_actions_logger_u7_manual_clear', array( $this, 'action_clear' ) );
-		add_action( 'logger_u7', array( $this, 'add' ) );
-		add_action( 'cl', array( $this, 'add' ) );
+			add_management_page(
+        $page_title = 'Logger',
+        $menu_title = 'Logger',
+        $capability = 'manage_options',
+        $menu_slug = 'logger_u7',
+        $func = array( __CLASS__, 'display_page') );
+		});
+
+		add_filter( "plugin_action_links_" . plugin_basename( __FILE__ ), array( __CLASS__, 'add_settings_link' ) );
+		add_action( 'tool_btns', array( __CLASS__, 'manual_clear' ), 15 );
+		add_action( 'tool_actions_logger_u7_manual_clear', array( __CLASS__, 'action_clear' ) );
+		add_action( 'logger_u7', array( __CLASS__, 'add' ) );
+		add_action( 'cl', array( __CLASS__, 'add' ) );
+
+
+    register_uninstall_hook( __FILE__, array(__CLASS__, 'uninstall') );
+
+
+
 	}
-	
+
+  /**
+   * Remove option if uninstall plugin
+   */
+  public static function uninstall() {
+    delete_option( 'logger_u7' );
+  }
+
 	/**
-	 *
+	 * Display Logger UI
 	 */
-	public function display_page() {
-		
+	public static function display_page() {
+
 		echo '<h1>Log</h1>';
 		if ( empty( $_GET['a'] ) ) {
-			
+
 			do_action( 'tool_btns' );
 		} else {
 			do_action( 'tool_actions_' . $_GET['a'] );
 		}
 		echo '<p>For adding data to log use hook: <br><pre>do_action("logger_u7", $var);</pre></p><hr>';
+
+    if(isset($GLOBALS['wp_object_cache'])){
+      $GLOBALS['wp_object_cache']->delete( 'logger_u7', 'options' );
+    }
+
 		$data = get_option( 'logger_u7' );
 		if ( ! is_array( $data ) ) {
 			echo '<p>No data in log</p>';
-			
+
 			return;
 		}
 		$data = array_reverse( $data );
@@ -72,15 +98,18 @@ class Logger_U7 {
 						<span><?php echo $item['timestamp']; ?></span>
 					</td>
 					<td>
-						<?php $this->vd( $item['data'] ); ?>
+						<?php self::vd( $item['data'] ); ?>
 					</td>
 				</tr>
 				<?php $i ++; endforeach; ?>
 		</table>
 		<?php
 	}
-	
-	public function vd( $var ) {
+
+  /**
+   * Vardump wrapper
+   */
+	public static function vd( $var ) {
 		echo '<pre>';
 		if ( ! empty( $var ) ) {
 			print_r( $var );
@@ -89,11 +118,16 @@ class Logger_U7 {
 		}
 		echo '</pre>';
 	}
-	
+
 	/**
-	 * @param string $data <pre><?php print_r( $item['data'] ) ?></pre>
+	 * Add var to log
 	 */
-	public function add( $data = '' ) {
+	public static function add( $data = '' ) {
+
+    if(isset($GLOBALS['wp_object_cache'])){
+      $GLOBALS['wp_object_cache']->delete( 'logger_u7', 'options' );
+    }
+
 		$log = get_option( 'logger_u7' );
 		if ( empty( $log ) ) {
 			$log = array();
@@ -103,49 +137,45 @@ class Logger_U7 {
 			'data'      => $data,
 		);
 		$log   = array_slice( $log, - 99, 99 );
+
+
+
 		update_option( 'logger_u7', $log, false );
 	}
-	
+
 	/**
 	 * Add fast link in plugins list
 	 */
-	public function add_settings_link( $links ) {
+	public static function add_settings_link( $links ) {
 		$settings_link = '<a href="tools.php?page=logger_u7">Logger</a>';
 		array_unshift( $links, $settings_link );
-		
+
 		return $links;
 	}
-	
+
 	/**
 	 *
 	 */
-	public function manual_clear() {
+	public static function manual_clear() {
 		?>
 		<a href="<?php echo add_query_arg( 'a', 'logger_u7_manual_clear', admin_url( 'tools.php?page=logger_u7' ) ) ?>"
 			class="button">Очистить</a>
 		<?php
 	}
-	
+
 	/**
-	 *
+	 * Clear log
 	 */
-	public function action_clear() {
-		
-		$data = $this->delete();
+	public static function action_clear() {
+
+    delete_option( 'logger_u7' );
+
 		if ( $data != false ) {
 			echo '<p>Очищено</p>';
-			wp_redirect( remove_query_arg( 'a', $this->url ) );
+			wp_redirect( remove_query_arg( 'a', self::$url ) );
 		}
 	}
-	
-	/**
-	 * @return bool
-	 */
-	public function delete() {
-		delete_option( 'logger_u7' );
-		
-		return true;
-	}
+
 }
 
-new Logger_U7;
+Logger::init();
